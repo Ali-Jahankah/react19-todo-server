032ec4b370beccbbaf64335ee96f89a6b9acb3f5
Ali Jahankhah <aral2@yahoo.com>
2025-07-03 13:40:10 +0100

    finish rotes and controllers

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -1,12 +1,16 @@
+import { ITodoCreateInput } from './../types/todos';
 import { Request, Response } from 'express';
 import prisma from '../prisma/client';
-import { ITodoCreateInput } from '../types/todos';
-export const getTodos = async (_req: Request, res: Response): Promise<void> => {
+import { Prisma, Todo } from '../generated/prisma';
+export const getTodos = async (
+  _req: Request,
+  res: Response<Todo[] | { error: string }>
+): Promise<void> => {
   try {
     const todos = await prisma.todo.findMany();
     res.json(todos);
   } catch (error) {
-    console.log('Faild to fetch todos', error);
+    console.log('Failed to fetch todos', error);
     res
       .status(500)
       .json({ error: 'Internal server error while fetching todos' });
@@ -14,7 +18,7 @@ export const getTodos = async (_req: Request, res: Response): Promise<void> => {
 };
 export const createTodo = async (
   req: Request<{}, {}, ITodoCreateInput>,
-  res: Response
+  res: Response<Todo | { error: string }>
 ): Promise<void> => {
   try {
     const { title, description, date, completed = false } = req.body;
@@ -35,5 +39,63 @@ export const createTodo = async (
   } catch (error) {
     console.log('Faild to create todo: ', error);
     res.status(500).json({ error: 'Internal server error in creating a todo' });
+  }
+};
+export const deleteTodo = async (
+  req: Request<{ id: string }>,
+  res: Response
+): Promise<void> => {
+  const { id } = req.params;
+  try {
+    await prisma.todo.delete({
+      where: {
+        id: Number(id)
+      }
+    });
+    res.status(204).send();
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === 'P2025'
+    ) {
+      res.status(404).json({ error: 'Todo not found!' });
+      return;
+    }
+    console.log('Faild to delete todo: ', error);
+    res.status(400).json({ error: 'Server error while deleting a todo' });
+  }
+};
+export const updateTodo = async (
+  req: Request<{ id: string }, {}, ITodoCreateInput>,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    if (data.date) {
+      data.date = new Date(data.date).toISOString();
+    }
+    const selectedTodo = await prisma.todo.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        ...data,
+        date: data.date ? new Date(data.date) : undefined
+      }
+    });
+    res.json(selectedTodo);
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === 'P2025'
+    ) {
+      res.status(404).json({ error: 'Todo not found!' });
+      return;
+    }
+    console.log('Faild to update todo ', error);
+    res
+      .status(500)
+      .json({ error: 'Internal server error while updating todo' });
   }
 };
--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
-import { getTodos, createTodo } from '../controllers/todoController';
+import {
+  getTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo
+} from '../controllers/todoController';
 
 const router = Router();
 
 router.get('/', getTodos);
 router.post('/', createTodo);
+router.put('/:id', updateTodo);
+router.delete('/:id', deleteTodo);
 
 export default router;
